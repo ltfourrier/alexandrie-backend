ae920e516d98e990488efedb15fdd49e9c3278a4
Laurent Fourrier <ltfourrier@gmail.com>
2021-01-09 16:01:09 +0100

    Refactored health endpoint.
    - /health route was changed to /v1/health.
    - Project structure added for routes.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+mod routes;
 
-#[get("/health")]
-async fn health() -> impl Responder {
-    HttpResponse::Ok().body("OK")
-}
+use routes::*;
+
+use actix_web::{web, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(health))
+    HttpServer::new(|| App::new().service(web::scope("/v1").service(get_health)))
         .bind("127.0.0.1:8080")?
         .run()
         .await
