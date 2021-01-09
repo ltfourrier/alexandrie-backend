785ab8634a3b02816f48f437e8eccf0f74756f4c
Laurent Fourrier <ltfourrier@gmail.com>
2021-01-09 16:09:31 +0100

    Migrated health endpoint to JSON.
    - Added serde as a dependency.

--- src/routes/health.rs
+++ src/routes/health.rs
@@ -1,6 +1,14 @@
-use actix_web::{get, Responder, HttpResponse};
+use actix_web::{get, HttpResponse, Result};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Health {
+    status: &'static str,
+}
 
 #[get("/health")]
-pub async fn get_health() -> impl Responder {
-    HttpResponse::Ok().body("OK")
+pub async fn get_health() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().json(Health {
+        status: "OK",
+    }))
 }
