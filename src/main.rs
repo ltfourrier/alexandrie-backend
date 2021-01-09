9e946cb926e8642a855d4cd8aae24825fd4bc683
Laurent Fourrier <ltfourrier@gmail.com>
2021-01-09 18:53:37 +0100

    Added logging capabilities.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@ use routes::*;
 use actix_web::{web, App, HttpServer};
 use anyhow::Result;
 use sqlx::postgres::PgPoolOptions;
+use simple_logger::SimpleLogger;
+use log::LevelFilter;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
+    SimpleLogger::new().with_level(LevelFilter::Info).init()?;
+
     let db_pool = PgPoolOptions::new()
         .max_connections(10)
         .connect("postgresql://alexandrie:password@localhost/alexandrie")
